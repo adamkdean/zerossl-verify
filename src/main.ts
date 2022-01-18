375be01d7d527718d608086a87895a652f6ad575
Adam K Dean <adamkdean@googlemail.com>
2022-01-18 13:34:10 +0000

    enhance logging considerably

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,8 @@ const app = express()
 
 app.use('*', (req, res, next) => {
   const datetime = new Date().toISOString()
-  console.log(`(${datetime}) ${req.method} ${req.path}`)
+  const host = req.headers['x-forwarded-host'] as string || req.headers.host
+  console.log(`[${host}] ${datetime} ${req.method} ${req.path}`)
   next()
 })
 
@@ -32,11 +33,17 @@ app.get('/', async (req, res) => {
 app.get('/.well-known/pki-validation/:value', async (req, res, next) => {
   // Find a certificate for the current hostname
   const host = req.headers['x-forwarded-host'] as string || req.headers.host
-  if (!host) return next()
+  if (!host) {
+    console.log('host is undefined')
+    return next()
+  }
 
   // Search certificate records for hostname
   const certs = await zerossl.listCertificates({ search: host, limit: 100 })
-  if (certs.result_count === 0) return next()
+  if (certs.result_count === 0) {
+    console.log(`no certificates found for hostname: ${host}`)
+    return next()
+  }
 
   // Find the correct certificate record
   let cert = null
@@ -45,16 +52,24 @@ app.get('/.well-known/pki-validation/:value', async (req, res, next) => {
     const validation = result.validation.other_methods[host]
     const requiredPath = validation.file_validation_url_http.replace(`http://${host}`, '')
     if (req.path === requiredPath) {
+      console.log(`found certificate ${result.id} for hostname: ${host}`)
+      console.log(`required path: ${requiredPath}`)
       cert = result
       break
     }
 
+    console.log(`skipping certificate ${result.id} (${result.common_name}, ${result.status})`)
     console.log(`${req.path} !== ${requiredPath}`)
   }
-  if (cert === null) return next()
+
+  if (cert === null) {
+    console.log(`no certificate found for hostname: ${host}`)
+    return next()
+  }
 
   // Return the validation data
   const content = cert.validation.other_methods[host].file_validation_content.join('\n')
+  console.log(`returning validation data for certificate ${cert.id}`)
   return res.contentType('text/plain').send(content)
 })
 
