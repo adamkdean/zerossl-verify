a0a221a794d78f1790fa882407cc05ae08c13389
Adam K Dean <adamkdean@googlemail.com>
2022-01-18 13:36:47 +0000

    fix request logging

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ const app = express()
 app.use('*', (req, res, next) => {
   const datetime = new Date().toISOString()
   const host = req.headers['x-forwarded-host'] as string || req.headers.host
-  console.log(`[${host}] ${datetime} ${req.method} ${req.path}`)
+  console.log(`[${host}] ${datetime} ${req.method} ${req.url}`)
   next()
 })
 
